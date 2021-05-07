0b52f598257672280fb831c589326beb12a000a6
KAIVIOR <wupeter99989@gmail.com>
2021-05-07 13:37:12 +0800

    Initial Commit

---
+++ part1/part1App.swift
@@ -0,0 +1,17 @@
+//
+//  part1App.swift
+//  part1
+//
+//  Created by 吳庭愷 on 2021/5/7.
+//
+
+import SwiftUI
+
+@main
+struct part1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
